7a5762ca56e744e4204d56d61c618db0f45e4229
amr852 <amrhaitham862@gmail.com>
2025-04-17 13:25:10 -0400

    reviews

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -126,3 +126,15 @@ def add_review(request):
             return JsonResponse({"status":401,"message":"Error in posting review"})
     else:
         return JsonResponse({"status":403,"message":"Unauthorized"})
+
+
+def add_review(request):
+    if(request.user.is_anonymous == False):
+        data = json.loads(request.body)
+        try:
+            response = post_review(data)
+            return JsonResponse({"status":200})
+        except:
+            return JsonResponse({"status":401,"message":"Error in posting review"})
+    else:
+        return JsonResponse({"status":403,"message":"Unauthorized"})
